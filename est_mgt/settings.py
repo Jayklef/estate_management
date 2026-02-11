ad94f88564a919c4514940348c6cf4228e7be1c9
Jayklef <arhawhojerry@gmail.com>
2026-02-11 23:57:43 +0000

    connected DB and Migrate script

--- est_mgt/settings.py
+++ est_mgt/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = "est_mgt.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "estmgtdb",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost"
     }
 }
 
