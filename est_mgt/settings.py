d0cf410ee7e600cbbcf1cb97c0cfbf41495a5b85
Jayklef <arhawhojerry@gmail.com>
2026-02-14 16:25:20 +0000

    Admin area customization - html/css

--- est_mgt/settings.py
+++ est_mgt/settings.py
@@ -135,3 +135,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# Media Folder setting
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
