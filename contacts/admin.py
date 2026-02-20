dc97484fca4fabb899e78040800a7735f529cb28
Jayklef <arhawhojerry@gmail.com>
2026-02-20 19:23:01 +0000

    Added contact form and form submission

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'listing', 'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listings')
+    list_per_page = 25
 
 # Register your models here.
+
+admin.site.register(Contact, ContactAdmin)
